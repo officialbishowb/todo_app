2c25c2210a81795e921ec73665aa34e04aab6028
officialbishowb <officialbishowb@gmail.com>
2022-09-12 22:14:46 +0200

    UI nearly finished

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/ui/task_ui.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: TaskScreen());
+  }
+}
---
+++ lib/model/task_brain.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+List<String> taskList() {
+  return const <String>["Task 1", "Task 2", "Task 3"];
+}
